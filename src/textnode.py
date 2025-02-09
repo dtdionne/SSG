3a93d0df641310bbf742799708a3a7be6403e868
dtdtionne <dtdionne@hotmail.com>
2025-02-09 09:06:58 -0600

    3.4

--- src/textnode.py
+++ src/textnode.py
@@ -23,3 +23,30 @@ class TextNode:
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+### REGEX C3.4
+
+import re
+
+def extract_markdown_images(text):
+    # Use the regex from the lesson to find image markdown patterns
+    pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
+    
+    # Return the matches
+    return matches
+
+# Simple test
+if __name__ == "__main__":
+    print("Testing extract_markdown_images...")
+    markdown_text = "This is text with an image: ![test image](https://example.com/image.png)"
+    print(extract_markdown_images(markdown_text))
+
+def extract_markdown_links(text):
+    # Your regex and logic here!
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+if __name__ == "__main__":
+    print("Testing extract_markdown_links...")
+    markdown_text = "This has a link [to Boot.dev](https://boot.dev) and [to Google](https://google.com)"
+    print(extract_markdown_links(markdown_text))
